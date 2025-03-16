ea75bd612cd2c25ec4245411f57149bb4e7fe47a
ChrisSaw112 <chssaw@ucdavis.edu>
2025-03-15 21:06:12 -0700

    major corrections to packet generation

--- sender_stop_and_wait_ChristopherSaw_921436261_MaxGao_919991682.py
+++ sender_stop_and_wait_ChristopherSaw_921436261_MaxGao_919991682.py
@@ -18,7 +18,6 @@ MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE
 @dataclasses.dataclass
 class Packet:
     seq_id: int
-    message: int
 
 # start measuring send time
 send_time = time.time()
@@ -37,49 +36,50 @@ with open('file.mp3', 'rb') as f: # read in mp3
     print(numpacketsNeed)
     print(os.path.getsize("file.mp3"))
     for i in range(numpacketsNeed):
-        nPacket = Packet(i, f.read(MESSAGE_SIZE))
+        seq_id = Packet(i)
+        nPacket = struct.pack("I", *(Packet.astuple(seq_id))) + f.read(MESSAGE_SIZE)
         packets_to_send.append(nPacket)
 
-print(packets_to_send.__sizeof__())
+def create_acknowledgement(seq_id, message):
+    return int.to_bytes(seq_id, SEQ_ID_SIZE, signed=True, byteorder='big') + message.encode()
 
+
+acknowledgement = create_acknowledgement(packets_to_send, 'ack')
+
+print(acknowledgement.decode())
+
+"""
+# send payload in chunks
+totalPacketDelay = 0
+chunk_size = 1024
+for packet in packets_to_send:
+    ackEd = False
+    per_packet_send = time.time()
+    while(ackEd == False):
+        client.sendto(packet, dest)
+        # receive server response
+        try:
+            response = client.recvfrom(1024)
+            throughput = response.decode()
+            
+            if()
+            ackEd = True
+        except socket.timeout:
+            print("Server response timed out. Resending")
+    per_packet_recv = time.time()
+    per_packet_rtt = per_packet_recv - per_packet_send
+    totalPacketDelay += per_packet_rtt
+
+client.close()
+"""
+
+
+
+""" For testing packet segmentation
 with open('file2.mp3', 'wb') as f:
     for i in range(numpacketsNeed):
         print(f'writing {packets_to_send[i].seq_id}')
         if(i == 0):
             print(packets_to_send[i].message)
         f.write(packets_to_send[i].message)
-"""
-# send header packet
-header_sent = False
-while not header_sent:
-    try:
-        client.sendto(header_packet, dest)
-        print("Header packet sent:", header)
-
-        # acknowledgment from the server
-        ack, _ = client.recvfrom(1024)
-        if ack.decode() == "Header Recieved":
-            print("Header packet acknowledged by server.")
-            header_sent = True
-    except socket.timeout:
-        print("Header packet lost, restarting...")
-        continue
-
-# send payload in chunks
-chunk_size = 1024
-for i in range(0, len(payload), chunk_size):
-    chunk = payload[i:i + chunk_size] 
-    client.sendto(chunk, dest)
-    
-
-# receive server response
-try:
-    response, server_address = client.recvfrom(1024)
-    throughput = response.decode()
-    print(f"Server response received from {server_address}:")
-    print(f"Throughput: {throughput} KB/s")
-except socket.timeout:
-    print("Server response timed out. Data may have been lost.")
-
-client.close()"
 """
