7b33a62e4a1a1e4254f29e6b70307ff7bf549b0c
ChrisSaw112 <chssaw@ucdavis.edu>
2025-03-16 13:40:09 -0700

    question 3

--- BGP.py
+++ BGP.py
@@ -9,8 +9,30 @@ print("Number of BGP RIBs: {}".format(len(bgp_data)))
 print()
 print("Sample BGP RIB: {}".format(bgp_data[0]))
 
-longestASPATH = ''
+longestASPATH = []
+longestSoFar = 0
 for entry in bgp_data:
-    if(entry['ASPATH'].__len__() > longestASPATH.__len__()):
-        longestASPATH = entry['ASPATH']
+    temp = []
+    tempCount = 0
+    for number in entry['ASPATH'].split(' '):
+        if(number not in temp):
+            tempCount += 1
+        temp.append(number)
+    if(tempCount > longestSoFar):
+        longestASPATH = temp
+        longestSoFar = tempCount
+
+print()
+print("longest path")
 print(longestASPATH)
+
+bgp_36992 = []
+for entry in bgp_data:
+    path = entry['ASPATH'].split(' ')
+    if(path[-1] == '36992'):
+        bgp_36992.append(entry)
+        print(entry)
+fieldnames = ['TIME','ORIGIN','FROM','SEQUENCE','ASPATH','PREFIX','NEXT_HOP']
+with open('bgp_36992.csv', 'w') as bgp2_file:
+    bgp_writer = csv.DictWriter(bgp2_file,fieldnames=fieldnames,delimiter=";")
+    bgp_writer.writerows(bgp_36992)
